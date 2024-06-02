f9554b869b733de9a7dd9d2c8ad3edceeba78579
student-vedansh <35373105+ebe25@users.noreply.github.com>
2024-06-02 22:27:36 +0530

    fix: named export Dashboard

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -75,7 +75,7 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip"
 
-export function Dashboard() {
+export default function Dashboard() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
@@ -222,13 +222,13 @@ export function Dashboard() {
           <Breadcrumb className="hidden md:flex">
             <BreadcrumbList>
               <BreadcrumbItem>
-                <Breadcrumba asChild>
+                <Breadcrumb >
                   <a href="#">Dashboard</a>
-                </Breadcrumba>
+                </Breadcrumb>
               </BreadcrumbItem>
               <BreadcrumbSeparator />
               <BreadcrumbItem>
-                <Breadcrumb asChild>
+                <Breadcrumb>
                   <a href="#">Orders</a>
                 </Breadcrumb>
               </BreadcrumbItem>
